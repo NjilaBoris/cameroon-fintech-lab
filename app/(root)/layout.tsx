f0c01c55086a80ab9c6cebc10736e3115ab84998
Njila Boris <njilaboris2@gmail.com>
2026-07-13 00:32:25 +0100

    final nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,14 +6,14 @@ import Nav from "@/components/Nav";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <main className="relative  overflow-x-hidden">
       <Nav />
 
       <section >
         {children}
       </section>
       <Footer />
-    </>
+    </main>
   );
 };
 

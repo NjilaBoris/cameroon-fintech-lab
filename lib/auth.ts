5d6a61958c4f7e7215d7b4094bd5fd3a1d00aea6
Njila Boris <njilaboris2@gmail.com>
2026-07-17 14:40:09 +0100

    imageconfig

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,20 +3,22 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
 import * as schema from "./db/schema";
 
-
 export const auth = betterAuth({
-  database: drizzleAdapter(db, {
-    provider: "pg",
-    schema,
-  }),
-  socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        schema,
+    }),
+    emailAndPassword: {
+        enabled: true,
     },
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
     },
-  },
 });
